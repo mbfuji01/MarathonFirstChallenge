942d711399096f4b8c0f0cae9b40ef8d2c99e509
Alex Asichka <sanechka3@mail.ru>
2023-02-06 19:07:51 +0200

    add Levels model

---
+++ MarathonFirstChallenge/Modules/MainGameModule/Controllers/LevelsModel.swift
@@ -0,0 +1,14 @@
+//
+//  LevelsModel.swift
+//  MarathonFirstChallenge
+//
+//  Created by Aleksandra Asichka on 2023-02-06.
+//
+
+import Foundation
+
+struct LevelsModel {
+    let numberOfQuestion: Int
+    let winAmount: Int
+    let image: String
+}
--- MarathonFirstChallenge/Modules/MainGameModule/Controllers/MainGameViewController.swift
+++ MarathonFirstChallenge/Modules/MainGameModule/Controllers/MainGameViewController.swift
@@ -11,15 +11,26 @@ import UIKit
 class MainGameViewController: UIViewController {
 	enum Constants {
 		static let anyVar: String = "anyVar"
-        static let testvalue: String = "test"
+        static let levelCell = "cell"
+        static let blueButton = "levels_blue_button"
+        static let darkButton = "levels_dark_blue_button"
+        static let greenButton = "levels_green_button"
+        static let yellowButton = "levels_yellow_button"
+        static let background = "background_image"
+        static let logo = "logo_image"
+        
 	}
 	
 	//MARK: - Create UI
-	
+	private let backgroundImage = UIImageView()
+    private let tableView = UITableView()
+    private var levels: [LevelsModel] = []
+    
 	//MARK: - Lifecycle
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+        levels = fetchData()
 		setupViews()
 		setConstraints()
 	}
@@ -33,4 +44,25 @@ class MainGameViewController: UIViewController {
 		
 		])
 	}
+}
+
+extension MainGameViewController {
+    func fetchData() -> [LevelsModel] {
+        let level1 = LevelsModel(numberOfQuestion: 1, winAmount: 500, image: Constants.darkButton)
+        let level2 = LevelsModel(numberOfQuestion: 2, winAmount: 1000, image: Constants.darkButton)
+        let level3 = LevelsModel(numberOfQuestion: 3, winAmount: 2000, image: Constants.darkButton)
+        let level4 = LevelsModel(numberOfQuestion: 4, winAmount: 3000, image: Constants.darkButton)
+        let level5 = LevelsModel(numberOfQuestion: 5, winAmount: 5000, image: Constants.blueButton)
+        let level6 = LevelsModel(numberOfQuestion: 6, winAmount: 7500, image: Constants.darkButton)
+        let level7 = LevelsModel(numberOfQuestion: 7, winAmount: 1000, image: Constants.darkButton)
+        let level8 = LevelsModel(numberOfQuestion: 8, winAmount: 12500, image: Constants.darkButton)
+        let level9 = LevelsModel(numberOfQuestion: 9, winAmount: 15000, image: Constants.darkButton)
+        let level10 = LevelsModel(numberOfQuestion: 10, winAmount: 25_000, image: Constants.blueButton)
+        let level11 = LevelsModel(numberOfQuestion: 11, winAmount: 50_000, image: Constants.darkButton)
+        let level12 = LevelsModel(numberOfQuestion: 12, winAmount: 100_000, image: Constants.darkButton)
+        let level13 = LevelsModel(numberOfQuestion: 13, winAmount: 250_000, image: Constants.darkButton)
+        let level14 = LevelsModel(numberOfQuestion: 14, winAmount: 500_000, image: Constants.darkButton)
+        let level15 = LevelsModel(numberOfQuestion: 15, winAmount: 1_000_000, image: Constants.yellowButton)
+        return [level15, level14, level13, level12, level11, level10, level9, level8, level7, level6, level5, level4, level3, level2, level1]
+    }
 }
