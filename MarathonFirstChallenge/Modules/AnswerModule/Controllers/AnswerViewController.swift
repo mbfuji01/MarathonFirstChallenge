01392fe3e701b0dfde4f5b98745ee19acc656d78
NSmolia <ipadsmolya@gmail.com>
2023-02-07 23:10:24 +0300

    Add back + UI
    Add background, some labels, buttons, images, constraints.

--- MarathonFirstChallenge/Modules/AnswerModule/Controllers/AnswerViewController.swift
+++ MarathonFirstChallenge/Modules/AnswerModule/Controllers/AnswerViewController.swift
@@ -1,20 +1,65 @@
 //
 //  AnswerViewController.swift
 //  MarathonFirstChallenge
-//
-//  Created by demasek on 05.02.2023.
-//
+
 
 import Foundation
 import UIKit
 
 class AnswerViewController: UIViewController {
 	enum Constants {
-		static let anyVar: String = "anyVar"
-        static let test: String = "Hello"
+        static let backgroundImage: String = "background_image"
+        
 	}
 	
 	//MARK: - Create UI
+    
+    private lazy var backgroundImageView: UIImageView = {
+    let imageView = UIImageView()
+        imageView.image = UIImage(named: Constants.backgroundImage)
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private lazy var questionNumber: UILabel = {
+        let questionNum = UILabel ()
+        questionNum.text = "QUESTION #1"
+        questionNum.textColor = .white
+        questionNum.alpha = 0.5
+        return questionNum
+    }()
+    
+    private lazy var allMoney: UILabel = {
+        let money = UILabel ()
+        money.text = "$500"
+        money.textColor = .white
+        return money
+    }()
+    
+    private lazy var timer: UIImageView = {
+        var secondsTimer = UIImageView()
+        secondsTimer.image = UIImage(named: "timer_image_regular")
+        secondsTimer.isUserInteractionEnabled = true
+        return timer
+    }()
+    
+    private lazy var seconds: UILabel = {
+        var time = UILabel ()
+        time.text = "24"
+        time.textColor = .white
+        return time
+    }()
+    
+    private lazy var questionText: UILabel = {
+        var text = UILabel ()
+        text.text = "How many continents are there on planet Earth?"
+        text.textColor = .white
+        return text
+    }()
+    
+    
+    
+    
 	
 	//MARK: - Lifecycle
 	
@@ -25,12 +70,25 @@ class AnswerViewController: UIViewController {
 	}
 	
 	private func setupViews() {
-		view.backgroundColor = .red
+        view.addSubview(backgroundImageView)
+        backgroundImageView.addSubview(questionNumber)
+//        view.addSubview(allMoney)
+//        view.addSubview(timer)
+//        timer.addSubview(seconds)
+//        view.addSubview(questionText)
+        
 	}
 	
 	private func setConstraints() {
+        backgroundImageView.frame = view.frame
 		NSLayoutConstraint.activate([
-		
+            
 		])
+        
+        questionNumber.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            questionNumber.centerXAnchor.constraint(equalTo: backgroundImageView.centerXAnchor),
+            questionNumber.centerYAnchor.constraint(equalTo: backgroundImageView.centerYAnchor)
+        ])
 	}
 }
