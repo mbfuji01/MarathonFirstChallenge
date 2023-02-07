da8df463c7cfde547ebdfb243cacceda35df38fe
MaidanovaSymbat <79370466+MaidanovaSymbat@users.noreply.github.com>
2023-02-08 04:03:40 +0600

    add some constraints

--- MarathonFirstChallenge/Modules/SplashModule/Controllers/SplashViewController.swift
+++ MarathonFirstChallenge/Modules/SplashModule/Controllers/SplashViewController.swift
@@ -16,28 +16,49 @@ class SplashViewController: UIViewController {
     //MARK: - Create UI
     
     private let imageView: UIImageView = {
-            let imageView = UIImageView()
-            imageView.image = UIImage(named: "background_image")
-            return imageView
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "background_image")
+        return imageView
         }()
     
+    private let logoView: UIImageView = {
+        let logogView = UIImageView()
+        logogView.image = UIImage(named: "logo_image")
+        return logogView
+    }()
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(imageView)
+        view.addSubview(logoView)
         setupViews()
         setConstraints()
+        
+       // DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2, execute: shouldPerformSegue(withIdentifier: "WelcomeViewController", sender: nil))
         
     }
     
     private func setupViews() {
-        //view.backgroundColor = .red
+        
     }
     
     private func setConstraints() {
-        NSLayoutConstraint.activate([
         
+        logoView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoView.heightAnchor.constraint(equalToConstant: 267),
+            logoView.widthAnchor.constraint(equalToConstant: 267),
+            logoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
 }
